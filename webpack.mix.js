eb4ecf514db5213f200d47586a71433645ac503e
Pim Hakkert <43008188+pimhakkert@users.noreply.github.com>
2021-04-15 15:21:39 +0200

    - Changed tailwind files

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,10 +12,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ]);
+     .postCss('resources/css/tailwind.css', 'public/css', [
+         require('postcss-import'),
+         require('tailwindcss'),
+     ]);
 
 if (mix.inProduction()) {
     mix.version();
