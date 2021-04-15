df6c03d3ad689b2a3e13b5a8393c0a8033784460
Pim Hakkert <43008188+pimhakkert@users.noreply.github.com>
2021-04-15 16:59:17 +0200

    - Made login work

--- resources/views/dashboard/auth/login.blade.php
+++ resources/views/dashboard/auth/login.blade.php
@@ -3,8 +3,28 @@
 @section('title', 'Login')
 
 @section('content')
+    <!-- TEMPORARY -->
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <!-- TEMPORARY -->
     <form method="POST" action="{{ route('login') }}">
         @csrf
+        <div>
+            <label for="email">E-mail</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button>LOGIN</button>
     </form>
 @endsection
 
