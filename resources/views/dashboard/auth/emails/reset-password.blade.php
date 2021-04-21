5f625cefb6e939034492610f6fac814a098bb1c9
pimhakkert <p.t.hakkert@outlook.com>
2021-04-21 21:28:06 +0200

    - Almost finish password reset e-mail message

--- resources/views/dashboard/auth/emails/reset-password.blade.php
+++ resources/views/dashboard/auth/emails/reset-password.blade.php
@@ -1,2 +1,27 @@
-Hi {{ $name }}, Please reset your password here. Click on the below link to reset the password.
-<a href="{{ $url }}">RESET</a>
+@extends('dashboard.layouts.dashboard-outside')
+
+@section('title', 'Password reset e-mail')
+
+@section('content')
+    <table id="email" class="ml-4 mt-14">
+        <tr>
+            <td><h1 class="italic text-brown text-5xl">Password reset request</h1></td>
+        </tr>
+        <tr>
+            <td><p class="mt-10">Hi {{ $name }},</p></td>
+        </tr>
+        <tr>
+            <td><p>Someone quested a password reset for the account connected to this e-mail address.<br>If this was a mistake, please ignore this e-mail and nothing will happen.</p></td>
+        </tr>
+        <tr>
+            <td><p>To reset your password, visit the following link:<br><a class="underline block" href="{{ $url }}">Click here to change your password!</a></p></td>
+        </tr>
+        <tr>
+            <td><p class="mt-8">Or copy the following url into your browser:<br>{{ $url }}</p></td>
+        </tr>
+        <tr>
+        <tr>
+            <td><p class="mt-8">Kind regards,<br>And-dreamers.com</p></td>
+        </tr>
+    </table>
+@endsection
