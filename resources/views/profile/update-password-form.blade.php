7ea06c128605e274b53fb8d904fdaff14a5a37a3
Brian Loman <brianloman@hotmail.com>
2021-05-18 22:39:23 +0200

    Styles admin + password

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -1,25 +1,24 @@
 <x-jet-form-section submit="updatePassword">
     <x-slot name="form">
-        <div>
-            <x-jet-label for="password" value="New Password" />
-            <x-jet-input id="password" type="password" wire:model.defer="state.password" autocomplete="new-password" />
-            <x-jet-input-error for="password"/>
+        <!-- New Password -->
+        <div class="bg-lightbrown flex flex-col relative mb-20" style="height: 422px;">
+            <div class="flex flex-col" style="padding: 20% 20% 0 20%">
+                <p class="text-5xl absolute" style="top: -18px; left: 30px;">PASSWORD</p>
+                <x-jet-label for="password" value="New Password" class="text-2xl"/>
+                <x-jet-input id="password" type="password" wire:model.defer="state.password" autocomplete="new-password" placeholder="New password" class="mb-6 bg-lightbrown border-0 focus:ring-0 placeholder-brown-light border-b-2 border-brown focus:border-brown"/>
+                <x-jet-input-error for="password"/>
+
+                <!-- Confirm Password -->
+                <x-jet-label for="password_confirmation" value="Confirm Password" class="text-2xl"/>
+                <x-jet-input id="password_confirmation" type="password" wire:model.defer="state.password_confirmation" autocomplete="new-password" placeholder="Confirm password" class="mb-6 bg-lightbrown border-0 focus:ring-0 placeholder-brown-light border-b-2 border-brown focus:border-brown"/>
+                <x-jet-input-error for="password_confirmation" />
+            </div>
+
+            <x-jet-button class="mt-auto border-brown border-4 pt-5 pb-3 text-2xl">
+                SAVE
+            </x-jet-button>
         </div>
 
-        <div>
-            <x-jet-label for="password_confirmation" value="Confirm Password" />
-            <x-jet-input id="password_confirmation" type="password" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
-            <x-jet-input-error for="password_confirmation" />
-        </div>
     </x-slot>
 
-    <x-slot name="actions">
-        <x-jet-action-message on="saved">
-            Saved.
-        </x-jet-action-message>
-
-        <x-jet-button>
-            Save
-        </x-jet-button>
-    </x-slot>
 </x-jet-form-section>
